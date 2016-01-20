967131660a763f977b3c24fa3732f0f6eb1de75f
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-01-20 14:20:16 -0700

    Added if statement to make HTTPs? yes or no

--- user-agent.php
+++ user-agent.php
@@ -1,92 +1,35 @@
+<link type="text/css" rel="stylesheet" href="css/style.css">
+
+<!--	//returns first forwarded IP match it finds-->
+
 <?php
-/**
- * Created by PhpStorm.
- * User: Tamra
- * Date: 1/20/2016
- * Time: 1:03 PM
- */
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8">
-    <title>What Is My IP?</title>
-  	<style type="text/css">
-	body {
-	margin: 0;
-	padding: 0;
-	background: #DFCC89;
-}
-    #main-content {
-      margin: 30px;
-      text-align: center;
-      color: #3D2399;
-    }
-    #main-content h1 {
-      font: 40px Arial, Helvetica, sans-serif;
-    }
-    #main-content p {
-      font: 24px "Times New Roman", Times, Georgia, serif;
-    }
-    #main-content p strong {
-      font-size: 70px;
-      color: #000000;
-    }
-  	</style>
-  </head>
-  <body>
-<?php
-
-// returns first forwarded IP match it finds
-function forwarded_ip() {
-	$keys = array(
-		'HTTP_X_FORWARDED_FOR',
-		'HTTP_X_FORWARDED',
-		'HTTP_FORWARDED_FOR',
-		'HTTP_FORWARDED',
-		'HTTP_CLIENT_IP',
-		'HTTP_X_CLUSTER_CLIENT_IP',
-	);
-
-	foreach($keys as $key) {
-		if(isset($_SERVER[$key])) {
-			$ip_array = explode(',', $_SERVER[$key]);
-			foreach($ip_array as $ip) {
-				$ip = trim($ip);
-				if(validate_ip($ip)) {
-					return $ip;
-				}
-			}
-		}
-	}
-	return '';
-}
-
-function validate_ip($ip) {
-	if(filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV4 | FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) === false) {
-		return false;
-	} else {
-		return true;
-	}
-}
-
-
-$remote_ip = $_SERVER['REMOTE_ADDR'];
-$forwarded_ip = forwarded_ip();
-
 ?>
-<div id="main-content">
-	<h1>What Is My IP?</h1>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Browser Mirror</title>
+	</head>
+	<body>
+		<div id="main-content">
+			<h1>Browser Mirror</h1>
+			<p><strong>Remote IP: </strong><?php echo($_SERVER['REMOTE_ADDR']); ?> </p>
+			<p><strong>User Agent: </strong><?php echo($_SERVER['HTTP_USER_AGENT']); ?> </p>
+			<p><strong>Referrer: </strong><?php echo($_SERVER['HTTP_REFERER']); ?> </p>
+			<p><strong>Request Time: </strong><?php echo($_SERVER['REQUEST_TIME']); ?> </p>
+			<p><strong>Request URI: </strong><?php echo($_SERVER['REQUEST_URI']); ?> </p>
+			<p><strong>Request Method: </strong><?php echo($_SERVER['REQUEST_METHOD']); ?> </p>
+			<p><strong>Query String: </strong><?php echo($_SERVER['QUERY_STRING']); ?> </p>
+			<p><strong>HTTP Accept: </strong><?php echo($_SERVER['HTTP_ACCEPT']); ?> </p>
+			<p><strong>Accept Charset: </strong><?php echo $_SERVER['HTTP_ACCEPT_CHARSET']; ?> </p>
+			<p><strong>Accept Encoding: </strong><?php echo($_SERVER['HTTP_ACCEPT_ENCODING']); ?> </p>
+			<p><strong>Accept Language: </strong><?php echo($_SERVER['HTTP_ACCEPT_LANGUAGE']); ?> </p>
+			<p><strong>HTTPS?: </strong><?php if(isset($_SERVER['HTTPS'])) {echo "Yes";}
+					else
+					{echo "No";} ?> </p>
+			<p><strong>Remote Port: </strong><?php echo($_SERVER['REMOTE_PORT']); ?> </p>
 
-	<p>The request came from:<br />
-		<strong><?php echo $remote_ip; ?></strong>
-	</p>
 
-	<?php if($forwarded_ip != '') { ?>
-		<p>The request was forwarded for:<br />
-			<strong><?php echo $forwarded_ip; ?></strong>
-		</p>
-	<?php } ?>
-
-</div>
-</body>
+		</div>
+	</body>
 </html>
