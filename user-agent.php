c342294727ec9b39d9e015a7ab904d2b4ebe4fc0
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-01-20 14:59:47 -0700

    Formatted the date from seconds to US format in PHP...my version and the better tutorial version.

--- user-agent.php
+++ user-agent.php
@@ -17,12 +17,17 @@
 			<p><strong>User Agent: </strong><?php echo($_SERVER['HTTP_USER_AGENT']); ?> </p>
 			<p><strong>Referrer: </strong><?php echo($_SERVER['HTTP_REFERER']); ?> </p>
 			<p><strong>Request Time: </strong>
-				<?php
+				<?php //my version working it out
 				$date = ($_SERVER['REQUEST_TIME']); //pulls the time from the server
-				$format= 'n/j/Y h:i;s a'; //formats the time
+				$format= 'l, F j, Y g:ia'; //formats the time day, month, date, year, time, am/pm
+				//timezone needs to be declared if not in php.ini file use dat.timezone = "America/New_York"
 				echo date($format, $date); // outputs the time
 				 ?> </p>
-
+			<?php date_default_timezone_set("American/New_York"); //not needed on my server but here for an example ?>
+			<p><strong>Request Time: </strong>
+				<?php //the much better version form the tutorial
+				echo date('l, F j, Y g:ia', $_SERVER['REQUEST_TIME']);
+				?> </p>
 			<p><strong>Request URI: </strong><?php echo($_SERVER['REQUEST_URI']); ?> </p>
 			<p><strong>Request Method: </strong><?php echo($_SERVER['REQUEST_METHOD']); ?> </p>
 			<p><strong>Query String: </strong><?php echo($_SERVER['QUERY_STRING']); ?> </p>
