21492c2d8b39fc29f7092ac7c1fdbf42fb0cdb8a
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-02-13 13:36:36 -0700

    Zend code to wrap your head around.

--- zend-questions.php
+++ zend-questions.php
@@ -9,7 +9,6 @@ function divideTwo($first, $second) {
 
     return($first / $second);
 }
-​
 try {
     divideTwo(42.0, "0");
 } catch(Exception $exception) { //(since Range Exception is a child of Exception it caught here. A better way to write this would be to have the more specific Range exception caught first.)
@@ -19,6 +18,7 @@ try {
 } catch(RangeException $range) {
     echo "Range Exception :" . $range->getMessage();
 }
+?>
 
 <?php //example 2
 function reverseArrayMembers($array) {
@@ -31,10 +31,10 @@ function reverseArrayMembers($array) {
 $array = ["foo", "bar", "baz"];
 reverseArrayMembers(array_flip($array));
 echo $array[1];
-
+?>
 
 //this should work
-
+<?php
 function reverseArrayMembers($array) {
     foreach($array as $key => $member) {
         $array[$key] = strrev($member);
@@ -49,6 +49,8 @@ print_r($array);
 
 
 //outputs yay
+?>
+
 <?php
 interface Logger {
     public function log($message);
@@ -73,7 +75,8 @@ enhancedLogger($bar, "yay");
 //answer
 //<p>yay
 //</p>
-
+?>
+<?php
 //new example
 interface Logger {
     public function log($message);
@@ -98,6 +101,7 @@ $bar = new Bar();
 $baz = new Baz();
 enhancedLogger($bar, "Back Paws for Bernie");
 enhancedLogger($baz, "Meow");
+?>
 
 <?php
 $closure = function foo($a, $b, $c) { //closures should not have names remove foo to get 3|2|1
@@ -105,6 +109,7 @@ $closure = function foo($a, $b, $c) { //closures should not have names remove fo
 };
 ​
 $closure(1, 2, 3);
+?>
 
 //parse error
 //Be prepared for purposeful and subtle syntax errors in Zend questions. Many take the format of:
@@ -116,3 +121,35 @@ $closure(1, 2, 3);
 //c) baz
 //d) parse error
 //e) fatal error
+
+
+//parse errors are syntactical
+//fatal error are logical problems see example on page 101
+//page 100 #2 diamond problem bc doSomething() and doSomething($special) PHP does not know which to use, so fatal error as well as problems with function naming specialFunction and mySpecialFunction
+
+<?php
+$numbers = [1, 2, 3];
+$words = ["foo", "bar", "meow"];
+$merged = array_merge($numbers, $words);
+//1,2,3, foo, bar, meow
+usort($merged, function($first, $second) { //user sort; good for sorting object
+    if(strlen($first) !== strlen($second)) {
+        return(strlen($second) - strlen($first)); //longer stings return positive and get prority
+    } else {
+        return($second - $first); //numeric subtraction or 0 for string - ewww = mean!!!! Strings = 0
+    }
+});
+print_r($merged);
+?>
+results
+Array
+(
+[0] => meow
+[1] => bar
+[2] => foo
+[3] => 3
+[4] => 2
+[5] => 1
+)
+
+
